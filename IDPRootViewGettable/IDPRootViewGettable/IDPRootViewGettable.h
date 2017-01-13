4976e85048d32aac1ee727bdb50187aabf27ade0
Artem Chabannyi <artem.chabanniy@gmail.com>
2017-01-13 12:08:34 +0200

    Added carthage dependency from IDPCastable.

---
+++ IDPRootViewGettable/IDPRootViewGettable/IDPRootViewGettable.h
@@ -0,0 +1,19 @@
+//
+//  IDPRootViewGettable.h
+//  IDPRootViewGettable
+//
+//  Created by Artem Chabannyi on 1/13/17.
+//  Copyright © 2017 IDAP Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IDPRootViewGettable.
+FOUNDATION_EXPORT double IDPRootViewGettableVersionNumber;
+
+//! Project version string for IDPRootViewGettable.
+FOUNDATION_EXPORT const unsigned char IDPRootViewGettableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDPRootViewGettable/PublicHeader.h>
+
+
