43f68cc606c0940d11ab2aa6aaac06bb7ce92891
Artem Chabannyi <artem.chabanniy@gmail.com>
2017-01-13 15:51:38 +0200

    Refactor folders strucrure. Added Country model.

---
+++ Tests/iOS/Source/Models/Country.swift
@@ -0,0 +1,36 @@
+//
+//  Country.swift
+//  iOS
+//
+//  Created by Artem Chabannyi on 1/13/17.
+//  Copyright © 2017 IDAP Group. All rights reserved.
+//
+
+import Foundation
+
+struct Country {
+    
+    let name: String
+    let topLevelDomain: [String]
+    let alpha2Code: String
+    let alpha3Code: String
+    let callingCodes: [String]
+    let capital: String
+    let altSpellings: [String]
+    let relevance: String
+    let region: String
+    let subregion: String
+    let translations: [String]
+    let population: Int
+    let latlng: [Float]
+    let demonym: String
+    let area: Float
+    let gini: Int
+    let timezones: String
+    let borders: [String]
+    let nativeName: String
+    let numericCode: String
+    let currencies: [String]
+    let languages: [String]
+    
+}
